00c69eb7598fda6858842faeea85df90b4288c1c
djrdunton-pocket <djrdunton@gmail.com>
2026-05-22 10:10:18 +0100

    git add . git commit -m "Add Plausible and GA4 analytics"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { DM_Sans, Fraunces } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 
 const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' });
 const fraunces = Fraunces({ subsets: ['latin'], variable: '--font-fraunces' });
@@ -33,7 +35,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${dmSans.variable} ${fraunces.variable}`}>
+      <head>
+        <Script
+          defer
+          src="https://plausible.io/js/pa-PbeuAfEJtpuArGNHr53ch.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()`}
+        </Script>
+      </head>
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-R85KHWL8SG" />
     </html>
   );
 }
